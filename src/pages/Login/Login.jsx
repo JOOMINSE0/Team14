0acdfd55914a82c72792e1cb1c7b64b7ac279775
nayeong lee <lny021102@gmail.com>
2023-11-02 04:15:29 +0900

    [ADD] 로그인 페이지 완성

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,65 @@
 import React from "react";
 import Nav from "../../component/Nav/Nav";
 import Header from "../../component/Header/Header";
+import { useState } from 'react';
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 //화면 Main(메인화면) 컴포넌트를 만든다
 const Login = () => {
+    const [inputText_1, setInputText_1] = useState(''); // 초기값 설정(이름)
+    const [inputText_2, setInputText_2] = useState(''); // 초기값 설정(날짜. 월)
+    const navigate = useNavigate(); // useNavigate 훅을 사용
+
+    const handleInputChange_1 = (e) => {
+        setInputText_1(e.target.value);
+    };
+
+    const handleInputChange_2 = (e) => {
+        setInputText_2(e.target.value);
+    };
+
+    const handleCompleteButtonClick = () => {
+        // 입력 완료 버튼 클릭 시 main 화면으로 이동
+        navigate('/main');
+    };
+
+    const handleCompleteButtonClick_signup = () => {
+        // 입력 완료 버튼 클릭 시 main 화면으로 이동
+        navigate('/signup');
+    };
+
 
     return (
         <div>
             <Header />
-            <div className="content">
-                <p>여기는 로그인 페이지입니다. 가족 일기장 기능을 사용할 때만 로그인하면 됩니다.</p>
+            <div className="content login-column">
+
+                <div>
+                    <p>아이디를 입력해주세요.</p>
+                    <input
+                        className="login-input"
+                        type="text"
+                        value={inputText_1}
+                        onChange={handleInputChange_1}
+                    />
+                    <div className="sizedbox"></div>
+                    <p>비밀번호를 입력해주세요.</p>
+                    <input
+                        className="login-input"
+                        type="text"
+                        value={inputText_2}
+                        onChange={handleInputChange_2}
+                    />
+                    <p>처음 이용하시는 건가요?</p>
+                    <button className="go-to-signup-button" onClick={handleCompleteButtonClick_signup}>
+                        회원가입 하러 가기
+                    </button>
+                    <div className="sizedbox"></div>
+                    <button className="login-button" onClick={handleCompleteButtonClick}>
+                        로그인 하기
+                    </button>
+                </div>
             </div>
             <Nav />
         </div>
