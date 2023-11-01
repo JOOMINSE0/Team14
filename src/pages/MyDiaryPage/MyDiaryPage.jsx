4a186d48c845a2d3eb2d1060138ae529423024b1
nayeong lee <lny021102@gmail.com>
2023-11-02 03:08:25 +0900

    [ADD] 개인 일기장 리스트 페이지 완성

--- src/pages/MyDiaryPage/MyDiaryPage.jsx
+++ src/pages/MyDiaryPage/MyDiaryPage.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import Nav from "../../component/Nav/Nav";
 import Header from "../../component/Header/Header";
+import './MyDiaryPage.scss';
 
 //화면 Main(메인화면) 컴포넌트를 만든다
 const MyDiaryPage = () => {
+    const divCount = 13; // 생성할 div의 개수
+    const divs = Array.from({ length: divCount }, (_, index) => index);
 
     return (
         <div>
             <Header />
-            <div className="content">
-                <p>여기는 개인 일기장, 즉 환자 또는 보호자 개인만 일기를 읽고 쓸 수 있는 페이지입니다.</p>
+            <div className="content my-diary-column">
+                <h2>가족들에게 고백하기 힘든 속마음을 적어보세요. 나만 볼 수 있는 일기장이에요.</h2>
+                <h2>오늘 찍은 사진, 내 모습을 촬영한 동영상도 함께 기록해보세요.</h2>
+                <div>
+                    {divs.map((index) => (
+                        <div
+                            key={index}
+                            className="my-diary"
+                        ></div>
+                    ))}
+                </div>
             </div>
+            <button className="fixed-button">
+                <span>+</span>
+            </button>
             <Nav />
         </div>
     );
