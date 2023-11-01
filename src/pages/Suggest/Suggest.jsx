cb1f669048cad4ee981b2cb68628f2d7a1cad29f
nayeong lee <lny021102@gmail.com>
2023-11-02 02:50:07 +0900

    [ADD] 추천 페이지 완성

--- src/pages/Suggest/Suggest.jsx
+++ src/pages/Suggest/Suggest.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Nav from "../../component/Nav/Nav";
 import Header from "../../component/Header/Header";
+import './Suggest.scss';
 
 //화면 Main(메인화면) 컴포넌트를 만든다
 const Suggest = () => {
+    const divCount = 13; // 생성할 div의 개수
+    const divs = Array.from({ length: divCount }, (_, index) => index);
 
     return (
         <div>
             <Header />
-            <div className="content">
-                <p>여기는 보험, 병원, 간병인, 사회제도를 추천해주는 페이지입니다.</p>
+            <div className="content suggest-column">
+                <h1>치매와 함께 살아가는 분들을 위해</h1>
+                <h1>사회 제도, 좋은 병원, 있으면 좋은 준비물들을 추천해드려요.</h1>
+                <div>
+                    {divs.map((index) => (
+                        <div
+                            key={index}
+                            className="custom-div"
+                        ></div>
+                    ))}
+                </div>
             </div>
             <Nav />
         </div>
