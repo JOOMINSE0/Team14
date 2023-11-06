63404f989fb22a185fe1b3b870954407e3764b7f
joominse0 <sally51501512@gmail.com>
2023-11-06 23:03:38 +0900

    초기페이지

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-//import Nav from "./component/Nav/Nav";
+import alarm from './img/alarm.png';
+import remember from './img/Remember.png';
+// import Nav from "./component/Nav/Nav";
 
 function App() {
   return (
-    <div>
+    <div id="iphone-frame">
+    <div id="logo-container">
+    <img id='logo' src={remember} alt="remember" />
+      <button id="alarm">
+        <img src={alarm} alt="alarm" style={{ width: '18px', height: '20px'}} />
+      </button>
+    </div>
+
+    <div id="line" style={{ width: '390px', height: '1px', backgroundColor: '#E5E5E5', margin: ' 10px 0 auto' }}></div>
+   
+
+    <div id="line" style={{ width: '390px', height: '1px', backgroundColor: '#E5E5E5', marginTop:'185%' }}></div>
 
     </div>
   );
