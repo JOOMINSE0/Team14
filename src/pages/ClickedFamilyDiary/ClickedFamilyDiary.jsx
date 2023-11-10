a014acf624a75c1441ccb9c8fc37265e8bedd53a
nayeong lee <lny021102@gmail.com>
2023-11-10 09:03:56 +0900

    일기장 콘텐츠 추가

--- src/pages/ClickedFamilyDiary/ClickedFamilyDiary.jsx
+++ src/pages/ClickedFamilyDiary/ClickedFamilyDiary.jsx
@@ -18,7 +18,13 @@ const ChooseFamilyDiary = () => {
             <Header />
             <div className="content clicked-family-diary-column">
                 <h2>이렇게 일기가 작성되었어요.</h2>
-                <div className="family-diary-pop"></div>
+                <div>
+                    <p>우리 가족 모두 힘내자!!</p>
+                </div>
+                <div className="sizedbox"></div>
+                <div className="family-diary-pop">
+                    우리 가족이 힘을 합치면 할머니의 치매를 이겨낼 수 있을거야. 우리 가족 사랑한다. 파이팅!
+                </div>
                 <div className="sizedbox"></div>
                 <button className="clicked-family-diary-button" onClick={handleCompleteButtonClick}>
                     우리 가족 일기장 목록으로 돌아가기
