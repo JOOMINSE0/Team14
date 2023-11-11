9c18c13b4978fe8cc5bc861bdac1c059e88f838d
joominse0 <sally51501512@gmail.com>
2023-11-11 17:37:27 +0900

    Splash수정

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -18,7 +18,7 @@ function Splash() {
     return (
         <div className="iphone-frame">
             <div className="splash-text">
-                <img src={remember_logo} alt="이미지" />
+                <img src={remember_logo} alt="이미지"  style={{width:'240px', height:'60px'}} />
                 <p>우리 가족을 기억해</p>
                 <p>소중한 나를 기억해</p>
             </div>
